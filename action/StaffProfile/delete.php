de034c6bdcd489a97813b35aa8ab04a7752b8604
kheavanna <kheavanna0@gmail.com>
2025-04-26 22:38:45 +0700

    Update Telegram
     Telegram_config

--- action/StaffProfile/delete.php
+++ action/StaffProfile/delete.php
@@ -10,6 +10,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
         // Start transaction
         $con->begin_transaction();
         
+
+        
         // Get photo and document paths before deleting
         $stmt = $con->prepare("SELECT Photo FROM hrstaffprofile WHERE EmpCode = ?");
         if (!$stmt) {
